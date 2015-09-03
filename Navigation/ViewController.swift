e0e122a18140101e5a14c24bb1ae970edb169275
Kartik Jain <kartikj2012@gmail.com>
2015-09-03 14:19:47 +0530

    Adding Button

--- Navigation/ViewController.swift
+++ Navigation/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func button(sender: AnyObject) {
+        println("hello")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
